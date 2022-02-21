08e6fe6d6206235844c3274e464a2b3ca41690c5
Tw93 <tw93@qq.com>
2022-02-21 20:04:06 +0800

    :bug: 差不多好勒

--- build.py
+++ build.py
@@ -3,12 +3,11 @@ readmefile=open('README.md','w')
 dir_ignore=['.git','.github']
 readmefile.write("# 潮流前端周刊\n")
 for filenames in os.walk('./md'):
-    #leaving the exceptional folders
-    print(filenames);
 
 print(filenames)
 # write relative link and names to readme.md
-for i in filenames:
-    readmefile.write('*  [{}]({}/{})\n'.format(i,key,i))
+for name in filenames:
+    if name.endswith('.md'):
+      readmefile.write('*  [{}]({}/{})\n'.format(name,'md',name))
 
 readmefile.close()
