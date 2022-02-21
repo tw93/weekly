8dfae855bb49fe7acc29b8962aea0ff4fad2e8b6
Tw93 <tw93@qq.com>
2022-02-21 19:49:40 +0800

    test

--- build.py
+++ build.py
@@ -1,67 +1,14 @@
-from python_graphql_client import GraphqlClient
-import feedparser
-import httpx
-import json
-import pathlib
-import re
 import os
-import datetime
+readmefile=open('README.md','w')
+dir_ignore=['.git','.github']
+readmefile.write("# 潮流前端周刊\n")
+for filenames in os.walk('./md'):
+    #leaving the exceptional folders
+    print(filenames);
 
-root = pathlib.Path(__file__).parent.resolve()
-client = GraphqlClient(endpoint="https://api.github.com/graphql")
+print(filenames)
+# write relative link and names to readme.md
+for i in filenames:
+    readmefile.write('*  [{}]({}/{})\n'.format(i,key,i))
 
-TOKEN = os.environ.get("GH_TOKEN", "")
-
-def replace_chunk(content, marker, chunk, inline=False):
-    r = re.compile(
-        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
-        re.DOTALL,
-    )
-    if not inline:
-        chunk = "\n{}\n".format(chunk)
-    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
-    return r.sub(chunk, content)
-
-def formatGMTime(timestamp):
-    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
-    return dateStr.date()
-
-def repository_get():
-    return """
-      repository(owner: "tw93", name: "weekly") {
-      defaultBranchRef {
-        name
-        target {
-          ... on Commit {
-            tree {
-              entries {
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-    """
-
-
-def fetch_files(oauth_token):
-    data = client.execute(
-      query = repository_get(),
-      headers={"Authorization": "Bearer {}".format(oauth_token)},
-    )
-    print()
-    print(">>>>>>>here\n")
-    print(data)
-    print(json.dumps(data, indent=4))
-    print()
-    return data
-
-if __name__ == "__main__":
-    readme = root / "README.md"
-    releases = fetch_files(TOKEN)
-    md = "\nhello,world"
-    readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "recent_releases", md)
-    readme.open("w").write(rewritten)
+readmefile.close()
