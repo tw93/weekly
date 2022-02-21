b16cc4aa2ce62730cdfdb3bba9e9add470069752
Tw93 <tw93@qq.com>
2022-02-21 20:33:18 +0800

    :bug: 简化一下逻辑

--- build.py
+++ build.py
@@ -2,12 +2,12 @@ import os
 import urllib.parse
 
 readmefile=open('README.md','w')
-dir_ignore=['.git','.github']
 readmefile.write("# 潮流前端周刊\n")
+
 for root, dirs, filenames in os.walk('./md'):
   filenames.sort(reverse=True)
   print(filenames)
-# write relative link and names to readme.md
+
 for name in filenames:
     if name.endswith('.md'):
       readmefile.write('*  [{}]({}/{})\n'.format(name,'md', urllib.parse.quote(name)))
