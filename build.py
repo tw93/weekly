835534c486565c34dc8998008f7a852628dd6911
Tw93 <tw93@qq.com>
2022-02-22 16:31:28 +0800

    :sparkles: import time

--- build.py
+++ build.py
@@ -1,15 +1,25 @@
 import os
+import datetime
 import urllib.parse
+
+def formatGMTime(timestamp):
+    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+    return dateStr.date()
 
 readmefile=open('README.md','w')
 readmefile.write("# 潮流前端周刊\n")
+recentfile=open('RECENT.md','w')
 
 for root, dirs, filenames in os.walk('./md'):
   filenames.sort(reverse=True)
-  print(filenames)
 
-for name in filenames:
+for index, name in filenames:
     if name.endswith('.md'):
-      readmefile.write('*  [{}]({}/{})\n'.format(name,'md', urllib.parse.quote(name)))
+      filepath   = 'https://github.com/tw93/weekly/tree/main/md' + urllib.parse.quote(filename)
+      modified = formatGMTime(os.path.getmtime(filepath))
+      if index < 6
+        recentfile.write('* [{}]({}) - {}\n'.format(name, filepath, modified))
+      readmefile.write('* [{}]({}/{})\n'.format(name,'md', urllib.parse.quote(name)))
 
 readmefile.close()
