aac82c0930f06608ae673d6083eb516ec3717008
Tw93 <tw93@qq.com>
2022-03-07 21:08:44 +0800

    fix

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
 import os
-import datetime
 import time
+import httpx
 import urllib.parse
 
-def formatTime(timestamp):
-    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+def fetch_ci_time(filePath):
+    entries = httpx.get("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
+    return time.strptime(entries[0].commit.committer.date,"%Y-%m-%d")
+
 
 if __name__ == "__main__":
   readmefile=open('README.md','w')
@@ -17,15 +18,14 @@ if __name__ == "__main__":
 
   for index, name in enumerate(filenames):
       if name.endswith('.md'):
-        filepath   = 'https://github.com/tw93/weekly/tree/main/md/' + urllib.parse.quote(name)
-        modified = time.ctime(os.path.getctime('md/'+name))
-        print(modified)
+        filepath = "/md/" + urllib.parse.quote(name)
+        url   = 'https://github.com/tw93/weekly/tree/main' + filepath
+        modified = fetch_ci_time(filepath)
         title = name.split('.md')[0]
-        # recentMd= '* [{}]({}) - {}\n'.format(title, filepath, modified)
-        readmeMd= '* [{}]({})\n'.format(title, filepath)
+        md= '* [{}]({}) - {}\n'.format(title, url, modified)
         if index < 6 :
-          recentfile.write(readmeMd)
-        readmefile.write(readmeMd)
+          recentfile.write(md)
+        readmefile.write(md)
 
   recentfile.close()
   readmefile.close()
