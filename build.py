89b2753be2a41f05ead030d6458ded6c0136460e
Tw93 <tw93@qq.com>
2022-03-07 21:19:18 +0800

    fix

--- build.py
+++ build.py
@@ -5,10 +5,10 @@ import urllib.parse
 
 def fetch_ci_time(filePath):
     entries = httpx.get("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
-    ciTime= entries.json()[0]
+    ciTime= entries.json()[0].commit.committer.date
     print(ciTime)
     return 1
-    return time.strptime(ciTime,"%Y-%m-%d")
+    # return time.strptime(ciTime,"%Y-%m-%d")
 
 
 if __name__ == "__main__":
@@ -25,8 +25,8 @@ if __name__ == "__main__":
         url   = 'https://github.com/tw93/weekly/tree/main' + filepath
         modified = fetch_ci_time(filepath)
         title = name.split('.md')[0]
-        recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
-        # readmeMd= '* [{}]({})\n'.format(title, url)
+        # recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
+        readmeMd= '* [{}]({})\n'.format(title, url)
         if index < 6 :
           recentfile.write(readmeMd)
         readmefile.write(readmeMd)
