db327d965cc7346866ffc1122738371987212737
Tw93 <tw93@qq.com>
2022-10-13 20:04:20 +0800

    :bug: 更新一下

--- build.py
+++ build.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
       if name.endswith('.md'):
         filepath = urllib.parse.quote(name)
         oldTitle = name.split('.md')[0]
-        url   = 'http://weekly.tw93.fun/post/' + oldTitle
+        url   = 'https://weekly.tw93.fun/post/' + oldTitle
         num = oldTitle.split('-')[0]
         title = '第' + num + '期 - ' + oldTitle.split('-')[1];
         readmeMd= '* [{}]({})\n'.format(title, url)
