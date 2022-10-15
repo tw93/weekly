1aa8f791657d9a02ed53a4b4f305c99fcb76c8b6
Tw93 <tw93@qq.com>
2022-10-15 23:29:00 +0800

    :bug: 更新一下

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
     title: '潮流周刊',
     author: 'Tw93',
-    description: '🩴 记录工程师 Tw93 的不枯燥生活，每周一发布',
+    description: '记录工程师 Tw93 的不枯燥生活，每周一发布',
     keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
     icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
     pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
