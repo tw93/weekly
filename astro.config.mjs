2c38c6562af3645b38f1f4cfa036922edafb75ee
Tw93 <tw93@qq.com>
2022-10-16 13:01:15 +0800

    :bug: 兼容个人特殊逻辑

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,68 @@
-import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import {parse} from 'node-html-parser';
 import dayjs from 'dayjs';
+import fs from 'fs';
+import { defineConfig } from 'astro/config';
+import { parse } from 'node-html-parser';
+import { SITE } from './src/config';
 
 function defaultLayoutPlugin() {
-    return function (tree, file) {
-        const headHtml = tree.children[0].value;
-        let image;
-        if (headHtml) {
-            image = parse(headHtml).querySelector('img').getAttribute('src');
-        }
-        file.data.astro.frontmatter.layout = '@layouts/post.astro';
-        file.data.astro.frontmatter.pic = image;
-        file.data.astro.frontmatter.desc = tree.children[1].children[1].value;
-        const num = file.history[0].split('/posts/')[1].split('-')[0];
+  return function (tree, file) {
+    const filePath = file.history[0];
+    file.data.astro.frontmatter.layout = '@layouts/post.astro';
+
+    // 头图放到文档中的第一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 pic 字段
+    if (tree.children[0]?.value) {
+      const imageElement = parse(tree.children[0].value).querySelector('img');
+      file.data.astro.frontmatter.pic = imageElement.getAttribute('src');
+    }
+
+    // 描述放到文档中头图的下一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 desc 字段
+    if (tree.children[1]?.children[1]?.value) {
+      file.data.astro.frontmatter.desc = tree.children[1].children[1].value;
+    }
+
+    const { date, desc, pic } = file.data.astro.frontmatter;
+
+    // 兼容没有描述情况
+    if (!desc) {
+      file.data.astro.frontmatter.desc = SITE.description;
+    }
+
+    // 兼容没有头图的情况
+    if (!pic) {
+      file.data.astro.frontmatter.pic = SITE.pic;
+    }
+
+    //这里也可以直接在 frontmatter，赋值给 date 字段
+    if (!date) {
+      const createDate = dayjs(fs.statSync(filePath).atime).format(
+        'YYYY/MM/DD',
+      );
+      //这里特殊处理了下，因为之前的weekly迁移过来后，createDate不对了，通过规律重写了下，99期以后的ok
+      if (SITE.repo == 'tw93/weekly') {
+        const num = filePath.split('/posts/')[1].split('-')[0];
         if (num < 99) {
-            file.data.astro.frontmatter.date = dayjs('2022-10-10')
-                .subtract(100 - num, 'week')
-                .format('YYYY/MM/DD');
+          file.data.astro.frontmatter.date = dayjs('2022-10-10')
+            .subtract(100 - num, 'week')
+            .format('YYYY/MM/DD');
         } else if (num == 99) {
-            file.data.astro.frontmatter.date = '2022-10-10';
+          file.data.astro.frontmatter.date = '2022-10-10';
         } else {
-            file.data.astro.frontmatter.date = dayjs().format('YYYY/MM/DD');
+          file.data.astro.frontmatter.date = createDate;
         }
-    };
+      } else {
+        file.data.astro.frontmatter.date = createDate;
+      }
+    }
+  };
 }
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()], markdown: {
-        remarkPlugins: [defaultLayoutPlugin], extendDefaultPlugins: true,
-    },
+  integrations: [react(), tailwind()],
+  markdown: {
+    remarkPlugins: [defaultLayoutPlugin],
+    extendDefaultPlugins: true,
+  },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-    title: '潮流周刊',
-    author: 'Tw93',
-    description: '记录工程师 Tw93 的不枯燥生活，每周一发布',
-    keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
-    icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
-    pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-    homePage: 'https://weekly.tw93.fun',
-    blogPage: 'https://tw93.fun/',
-    twitterId: 'HiTw93',
-    githubId: 'tw93',
-    repo: 'tw93/weekly'
+  title: '潮流周刊',
+  author: 'Tw93',
+  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
+  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
+  pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
+  homePage: 'https://weekly.tw93.fun',
+  blogPage: 'https://tw93.fun/',
+  twitterId: 'HiTw93',
+  githubId: 'tw93',
+  repo: 'tw93/weekly'
 };
 
