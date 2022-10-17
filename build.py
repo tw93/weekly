59db9465d33267343c1590e0f237bc8313a3717e
Tw93 <tw93@qq.com>
2022-10-17 10:25:55 +0800

    :bug: 更新一下

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import urllib.parse
 import datetime
 
 def fetch_ci_time(filePath):
+    print("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
     entries = httpx.get("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
     ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
@@ -17,6 +18,8 @@ if __name__ == "__main__":
 
   for root, dirs, filenames in os.walk('./src/pages/posts'):
     filenames = sorted(filenames, key=lambda x:float(re.findall("(\d+)",x)[0]), reverse=True)
+    print(filenames)
+
   for index, name in enumerate(filenames):
       if name.endswith('.md'):
         filepath = urllib.parse.quote(name)
