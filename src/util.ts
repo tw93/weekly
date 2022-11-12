4f10f72b7f503ecf49d672302be10963f03da92d
Tw93 <tw93@qq.com>
2022-11-12 22:33:58 +0800

    :sparkles: 周刊支持上下一页

--- src/util.ts
+++ src/util.ts
@@ -1,19 +1,25 @@
 // 转化成标题
 export const parseTitle = (currentPage: string) => {
-    const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
-    let title = '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
-    if (title.slice(-1) == '/') {
-        title = title.substring(0, title.length - 1);
-    }
-    return title
+  const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
+  let title = '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
+  if (title.slice(-1) == '/') {
+    title = title.substring(0, title.length - 1);
+  }
+  return title
+}
+
+//获取当前文章的序号
+export const getIndex = (currentPage: string) => {
+  const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
+  return parseInt(oldTitle.split('-')[0])
 }
 
 //排序所有的文章
 export const sortPosts = (allPosts: any) => {
-    return allPosts.sort((a, b) => {
-        return (
-            parseInt(b.url.split('/posts/')[1].split('-')[0]) -
-            parseInt(a.url.split('/posts/')[1].split('-')[0])
-        );
-    });
+  return allPosts.sort((a, b) => {
+    return (
+      parseInt(b.url.split('/posts/')[1].split('-')[0]) -
+      parseInt(a.url.split('/posts/')[1].split('-')[0])
+    );
+  });
 }
