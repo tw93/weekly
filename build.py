b716b43f15a84789c8d15bedbc63881cfdde1a75
Tw93 <tw93@qq.com>
2023-10-09 14:10:22 +0800

    :bug: update

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ import datetime
 
 def fetch_ci_time(filePath):
     entries = httpx.get("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
+    print("entries>>> " + entries)
     ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
     # return datetime.datetime.strptime(ciTime,"%Y-%m-%d")
