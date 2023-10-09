b9998b209165a779ac1852ccb28f744f2237907a
Tw93 <tw93@qq.com>
2023-10-09 14:30:51 +0800

    :bug: fix auto generate

--- build.py
+++ build.py
@@ -6,7 +6,6 @@ import datetime
 
 def fetch_ci_time(filePath):
     entries = httpx.get("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
-    print("entries>>> ", "https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
     ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
     # return datetime.datetime.strptime(ciTime,"%Y-%m-%d")
