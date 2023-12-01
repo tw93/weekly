e46be417ebcd27509ae0d03da0b2c033fff83da2
Tw93 <tw93@qq.com>
2023-12-01 18:40:58 +0800

    :sparkles: Update astro to the latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+import fs from 'fs';
+import dayjs from 'dayjs';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import dayjs from 'dayjs';
-import fs from 'fs';
 import { defineConfig } from 'astro/config';
 import { parse } from 'node-html-parser';
 import { SITE } from './src/config';
@@ -12,11 +12,10 @@ function defaultLayoutPlugin() {
     file.data.astro.frontmatter.layout = '@layouts/post.astro';
 
     // 头图放到文档中的第一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 pic 字段
-    if (tree.children[0]?.value) {
+    if (tree.children[0]?.value && !file.data.astro.frontmatter.pic) {
       const imageElement = parse(tree.children[0].value).querySelector('img');
       file.data.astro.frontmatter.pic = imageElement.getAttribute('src');
     }
-    console.log(tree.children[1]?.children)
 
     // 描述放到文档中头图的下一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 desc 字段
     if (tree.children[1]?.children[1]?.value) {
@@ -38,8 +37,9 @@ function defaultLayoutPlugin() {
     //这里也可以直接在 frontmatter，赋值给 date 字段
     if (!date) {
       const createDate = dayjs(fs.statSync(filePath).birthtime).format('YYYY/MM/DD');
+
       //这里特殊处理了下，因为之前的weekly迁移过来后，createDate不对了，通过规律重写了下，100期以后直接读取
-      if (SITE.repo == 'tw93/weekly') {
+      if (SITE.repo === 'tw93/weekly') {
         const num = filePath.split('/posts/')[1].split('-')[0];
         if (num < 100) {
           file.data.astro.frontmatter.date = dayjs('2022-10-10')
@@ -48,6 +48,7 @@ function defaultLayoutPlugin() {
         } else {
           file.data.astro.frontmatter.date = createDate;
         }
+
         //对于110期以后的，由于原有封面图不支持twitter，这里兼容一下
         if (num >= 110) {
           file.data.astro.frontmatter.twitterImg = `https://weekly.tw93.fun/assets/${num}.jpg`;
@@ -64,6 +65,5 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   markdown: {
     remarkPlugins: [defaultLayoutPlugin],
-    extendDefaultPlugins: true,
   },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 };
