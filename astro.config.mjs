4f98c1117cbc1270fa9358c15f97a40256750278
Tw93 <tw93@qq.com>
2024-01-21 22:14:13 +0800

    :sparkles: newpost162

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,61 +6,56 @@ import { defineConfig } from 'astro/config';
 import { parse } from 'node-html-parser';
 import { SITE } from './src/config';
 
+const DEFAULT_FORMAT = 'YYYY/MM/DD';
+const WEEKLY_REPO_NAME = 'tw93/weekly';
+
+function getCreateDateFormat(filePath) {
+  return dayjs(fs.statSync(filePath).birthtime).format(DEFAULT_FORMAT);
+}
+
+function getWeeklyDateFormat(num) {
+  if (num < 100) {
+    return dayjs('2022-10-10')
+      .subtract(100 - num, 'week')
+      .format(DEFAULT_FORMAT);
+  }
+  return getCreateDateFormat(filePath);
+}
+
+function getTwitterImg(num) {
+  return num >= 110 ? `https://weekly.tw93.fun/assets/${num}.jpg` : undefined;
+}
+
 function defaultLayoutPlugin() {
   return function (tree, file) {
     const filePath = file.history[0];
-    file.data.astro.frontmatter.layout = '@layouts/post.astro';
+    const { frontmatter } = file.data.astro;
+    frontmatter.layout = '@layouts/post.astro';
 
-    // 头图放到文档中的第一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 pic 字段
-    if (tree.children[0]?.value && !file.data.astro.frontmatter.pic) {
+    if (tree.children[0]?.value && !frontmatter.pic) {
       const imageElement = parse(tree.children[0].value).querySelector('img');
-      file.data.astro.frontmatter.pic = imageElement.getAttribute('src');
+      frontmatter.pic = imageElement.getAttribute('src');
     }
 
-    // 描述放到文档中头图的下一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 desc 字段
     if (tree.children[1]?.children[1]?.value) {
-      file.data.astro.frontmatter.desc = tree.children[1].children[1].value;
+      frontmatter.desc = tree.children[1].children[1].value;
     }
 
-    const { date, desc, pic } = file.data.astro.frontmatter;
+    frontmatter.desc = frontmatter.desc || SITE.description;
+    frontmatter.pic = frontmatter.pic || SITE.pic;
 
-    // 兼容没有描述情况
-    if (!desc) {
-      file.data.astro.frontmatter.desc = SITE.description;
+    if (!frontmatter.date) {
+      frontmatter.date = SITE.repo === WEEKLY_REPO_NAME
+        ? getWeeklyDateFormat(filePath.split('/posts/')[1].split('-')[0])
+        : getCreateDateFormat(filePath);
     }
 
-    // 兼容没有头图的情况
-    if (!pic) {
-      file.data.astro.frontmatter.pic = SITE.pic;
-    }
-
-    //这里也可以直接在 frontmatter，赋值给 date 字段
-    if (!date) {
-      const createDate = dayjs(fs.statSync(filePath).birthtime).format('YYYY/MM/DD');
-
-      //这里特殊处理了下，因为之前的weekly迁移过来后，createDate不对了，通过规律重写了下，100期以后直接读取
-      if (SITE.repo === 'tw93/weekly') {
-        const num = filePath.split('/posts/')[1].split('-')[0];
-        if (num < 100) {
-          file.data.astro.frontmatter.date = dayjs('2022-10-10')
-            .subtract(100 - num, 'week')
-            .format('YYYY/MM/DD');
-        } else {
-          file.data.astro.frontmatter.date = createDate;
-        }
-
-        //对于110期以后的，由于原有封面图不支持twitter，这里兼容一下
-        if (num >= 110) {
-          file.data.astro.frontmatter.twitterImg = `https://weekly.tw93.fun/assets/${num}.jpg`;
-        }
-      } else {
-        file.data.astro.frontmatter.date = createDate;
-      }
+    if (SITE.repo === WEEKLY_REPO_NAME) {
+      frontmatter.twitterImg = getTwitterImg(filePath.split('/posts/')[1].split('-')[0]);
     }
   };
 }
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   markdown: {
