a9debee2d618b9b64b02daf2e7e542744755e511
Tw93 <tw93@qq.com>
2024-04-02 14:08:47 +0800

    :bug: No optimization for gif svg images

--- rehype-customize-image-src.js
+++ rehype-customize-image-src.js
@@ -4,13 +4,15 @@ import { visit } from 'unist-util-visit';
 export default function rehypeCustomizeImageSrc() {
 	return (tree) => {
 		visit(tree, 'raw', (node) => {
-			// Match <img> tags using regular expressions
+			// Match <img> tags excluding .gif and .svg images using regular expressions
 			const imgRegex =
-				/<img src="(https:\/\/cdn\.fliggy\.com\/[^"]+|https:\/\/gw\.alipayobjects\.com\/[^"]+)"/g;
+				/<img src="(https:\/\/cdn\.fliggy\.com\/[^"]+|https:\/\/gw\.alipayobjects\.com\/[^"]+)(?<!\.gif|\.svg|\.GIF|\.SVG)"/g;
 			// Replace the src attribute of the matched <img> tag
 			if (imgRegex.test(node.value)) {
 				node.value = node.value.replace(imgRegex, (match, p1) => {
-					return `<img src="${p1}?x-oss-process=image/resize,w_3600/format,webp"`;
+					// Check if the URL already contains query parameters
+					const separator = p1.includes('?') ? '&' : '?';
+					return `<img src="${p1}${separator}x-oss-process=image/resize,w_3600/format,webp"`;
 				});
 			}
 		});
