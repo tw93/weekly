6e0354355abcf82e8b9664ebb334d5d9536e3300
Tw93 <tw93@qq.com>
2024-12-15 15:31:05 +0800

    :pencil: newpost204

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import os
 import httpx
+import json
 import re
 import urllib.parse
 
@@ -10,20 +11,23 @@ def fetch_ci_time(file_path):
     return ci_time
 
 if __name__ == "__main__":
-    with open('README.md', 'w') as readme_file, open('RECENT.md', 'w') as recent_file:
+    with open('README.md', 'w') as readme_file, open('RECENT.md', 'w') as recent_file, open('posts.json', 'w') as posts_file:
         readme_file.write("# 潮流周刊\n\n> 记录工程师 Tw93 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/tw93/weekly/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
 
         for root, dirs, filenames in os.walk('./src/pages/posts'):
             filenames = sorted(filenames, key=lambda x: float(re.findall(r"(\d+)", x)[0]), reverse=True)
 
+        posts = []
         for index, name in enumerate(filenames):
             if name.endswith('.md'):
                 file_path = urllib.parse.quote(name)
                 old_title = name.split('.md')[0]
-                url = f'https://weekly.tw93.fun/posts/{old_title}'
-                title = f'第 {old_title.split("-")[0]} 期 - {old_title.split("-")[1]}'
-                readme_md = f'* [{title}]({url})\n'
                 num = int(old_title.split('-')[0])
+                short_title = old_title.split('-')[1]
+                url = f'https://weekly.tw93.fun/posts/{old_title}'
+                title = f'第 {num} 期 - {short_title}'
+                readme_md = f'* [{title}]({url})\n'
+                posts.append({"num": num, "title": short_title, "url": url})
 
                 if index < 5:
                     modified = fetch_ci_time(f'/src/pages/posts/{file_path}')
@@ -31,3 +35,5 @@ if __name__ == "__main__":
                     recent_file.write(recent_md)
 
                 readme_file.write(readme_md)
+
+        json.dump(posts, posts_file, ensure_ascii=False, indent=2)
